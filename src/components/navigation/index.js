1059a4799b844eda84f6d7557109f291fbdf327d
panyexiong <46130795+panyexiong@users.noreply.github.com>
2022-06-30 02:52:18 +0000

    nav 隐藏

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -4,22 +4,26 @@ export default {
     name: 'navigation',
     data() {
         return {
-            items: ['money', 'time', 'others']
+            items: ['money', 'time', 'others'],
+            show_items: false,
         }
     },
     render(h) {
         return h('div', {
-            staticClass: 'nav'
+            staticClass: 'nav',
+            on: {
+                mouseenter: () => this.show_items = true,
+                mouseleave: () => this.show_items = false,
+            }
         }, [
             this.render_logo(h),
             this.render_items(h),
-
         ])
     },
     methods: {
         render_logo(h) {
             return h('div', {
-                staticClass: 'div__img--logo',
+                staticClass: 'div__img--logo'
             }, [
                 h('img', {
                     staticClass: 'logo',
@@ -30,6 +34,7 @@ export default {
             ])
         },
         render_items(h) {
+            if (!this.show_items) return null;
             return h('div', {
                 staticClass: 'nav__items'
             }, [
